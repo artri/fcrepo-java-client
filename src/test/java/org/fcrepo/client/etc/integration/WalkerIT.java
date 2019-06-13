d5593e76f057d97f67f273cf75e7b34d95aa9518
Artem Trybrat <artem.trybrat@thomsonreuters.com>
2019-06-13 18:49:03 +0300

    FCR-01: simplified project structure

--- src/test/java/org/fcrepo/client/etc/integration/WalkerIT.java
+++ src/test/java/org/fcrepo/client/etc/integration/WalkerIT.java
@@ -17,53 +17,68 @@
  */
 package org.fcrepo.client.etc.integration;
 
-import org.apache.commons.io.IOUtils;
-import org.fcrepo.client.FcrepoOperationFailedException;
-import org.fcrepo.client.FcrepoResponse;
-import org.fcrepo.client.etc.Walker;
-import org.fcrepo.client.etc.WalkerConfig;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import static javax.ws.rs.core.Response.Status.CREATED;
 import static org.fcrepo.client.etc.Walker.TEXT_TURTLE;
 import static org.junit.Assert.assertEquals;
 
+import org.apache.commons.io.IOUtils;
+import org.fcrepo.client.FcrepoClient;
+import org.fcrepo.client.FcrepoOperationFailedException;
+import org.fcrepo.client.FcrepoResponse;
+import org.fcrepo.client.TestUtils;
+import org.fcrepo.client.etc.Walker;
+import org.fcrepo.client.etc.WalkerConfig;
+import org.fcrepo.client.integration.AbstractResourceIT;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.URI;
+
 /**
  * @author awoods
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring-test/test-container.xml")
 public class WalkerIT extends AbstractResourceIT {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WalkerIT.class);
 
+    private static final WalkerConfig WALKER_CONFIG = new WalkerConfig(serverAddress, "fedoraAdmin", "password");
+    private FcrepoClient client;
     private Walker walker;
 
-    @Before
-    public void setUp() {
-        super.setUp();
-        final WalkerConfig config = new WalkerConfig(serverAddress, "fedoraAdmin", "password");
-        walker = new Walker(config);
+    
+    public WalkerIT() {
+        super();
+
+        walker = new Walker(WALKER_CONFIG);
+        client = FcrepoClient.client()
+                .credentials("fedoraAdmin", "password")
+                .authScope("localhost")
+                .build();
     }
 
     @Test
-    public void testGetChildren() throws URISyntaxException, FcrepoOperationFailedException, IOException {
+    public void testGetChildren() throws Exception {
         // Create some children
         final int numChildren = 4;
         for (int x = 0; x < numChildren; ++x) {
-            postResource(new ByteArrayInputStream(rdfTtl.getBytes()));
+            postResource(new ByteArrayInputStream(TestUtils.rdfTtl.getBytes()));
         }
 
         // Verify all children plus the base are found
         assertEquals(numChildren + 1, walker.run());
     }
 
-    private void postResource(final InputStream body) throws IOException, FcrepoOperationFailedException {
-        try (final FcrepoResponse response = client.post(URI.create(serverAddress)).body(body, TEXT_TURTLE).perform()) {
+    private void postResource(final InputStream body) throws Exception {
+        try (final FcrepoResponse response = client.post(new URI(serverAddress)).body(body, TEXT_TURTLE).perform()) {
             final String content = IOUtils.toString(response.getBody(), "UTF-8");
+            LOGGER.info("Received response: {}", content);
             final int status = response.getStatusCode();
             assertEquals("Didn't get a CREATED response! Got content:\n" + content, CREATED.getStatusCode(), status);
         }
